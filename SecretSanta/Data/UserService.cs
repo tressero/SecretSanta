11204368be4dae94f016cdb21c94c22d69b3ef31
mochsner <marc@ochsner.me>
2020-11-07 14:48:38 -0600

    #1 - Initial SecretSanta

---
+++ SecretSanta/Data/UserService.cs
@@ -0,0 +1,23 @@
+namespace SecretSanta.Data
+{
+    public class UserService
+    {
+        private static NUMBER_OF_USERS = 3;
+            
+        private static readonly int[] Pointers = new[]
+        {
+            for 
+        };
+
+        public Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
+        {
+            var rng = new Random();
+            return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = startDate.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            }).ToArray());
+        }
+    }
+}
